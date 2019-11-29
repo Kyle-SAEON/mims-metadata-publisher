a6bc119dacef34c18c05e3ca9b2777c358534c94
mcalisterb@gmail.com <mcalisterb@gmail.com>
2019-11-29 12:53:05 +0200

    reworking importer with latest spreadsheet format

--- mims_excel_importer.py
+++ mims_excel_importer.py
@@ -9,14 +9,20 @@ class RecordParseError(Exception):
 
 class MIMSExcelImporter:
     _required_columns = \
-        ['ID','AlternateID MIMS full accession', 'DOI', 'Publication Date',\
-         'MetaData Standard', 'Metadata date stamp', 'Access', 'Project / Collection',\
-         'Title','Cited Authors','Responsible parties (Contributors)','Publisher',\
-         'Keywords (free text)','Keywords (Data types CV)','Category','Abstract',\
-         'Language','Format','Spatial Representation Type','Spatial Resolution',\
-         'Reference System','Resource Type','Location (CV)','Bounding Box',\
-         'Vertical Extent','Start Date','End Date','License','Rights URL',\
-         'Lineage','Online Resource','Supplementary Links','Related','Notes']
+        ['fileIdentifier', 'DOI', 'date', 'metadataStandardName', 'metadataStandardVersion', \
+         'metadataTimestamp', 'accessConstraints', 'descriptiveKeywords', 'title', 'responsibleParties', \
+         'responsibleParties.1','responsibleParties.2','keyword', 'topicCategories', 'abstract', 'languages', \
+         'formatName', 'spatialRepresentationType', 'spatialResolution', 'referenceSystemName', 'scope', \
+         'geographicIdentifier', 'boundingBox', 'verticalElement', 'startTime', 'endTime', 'rights', \
+         'rightsURI', 'lineageStatement', 'onlineResources', 'relatedIdentifiers']
+        #['ID','AlternateID MIMS full accession', 'DOI', 'Publication Date',\
+        # 'MetaData Standard', 'Metadata date stamp', 'Access', 'Project / Collection',\
+        # 'Title','Cited Authors','Responsible parties (Contributors)','Publisher',\
+        # 'Keywords (free text)','Keywords (Data types CV)','Category','Abstract',\
+        # 'Language','Format','Spatial Representation Type','Spatial Resolution',\
+        # 'Reference System','Resource Type','Location (CV)','Bounding Box',\
+        # 'Vertical Extent','Start Date','End Date','License','Rights URL',\
+        # 'Lineage','Online Resource','Supplementary Links','Related','Notes']
 
     def __init__(self):
         pass
@@ -35,7 +41,7 @@ class MIMSExcelImporter:
                     self.parse_raw_record(raw_record)
                     raw_records.append(raw_record)
                 except RecordParseError as e:
-                    print("{}    Record id: {}".format(e, raw_record['ID']))                    
+                    print("{}    Record id: {}".format(e, raw_record['fileIdentifier']))                    
         except Exception as e:
             print("Error while reading excel speadsheet. {}".format(e))
             traceback.print_exc(file=sys.stdout)
@@ -46,36 +52,59 @@ class MIMSExcelImporter:
             if col not in self._required_columns:
                 raise Exception("Invalid column found: {}".format(col))
         # parse where necessary
-        self.parse_contact_details(record, 'Cited Authors')
-        self.parse_contact_details(record, 'Responsible parties (Contributors)')   
-        self.parse_column_list(record, 'Keywords (free text)')
-        self.parse_column_list(record, 'Category')
-        self.parse_column_list(record, 'Format')
-        self.parse_bounding_box(record)
+        self.parse_responsible_parties(record, 'responsibleParties')
+        self.parse_responsible_parties(record, 'responsibleParties.1',True)
+        self.parse_responsible_parties(record, 'responsibleParties.2',True)
 
-    def parse_contact_details(self, record, contact_type):
-        if contact_type not in record:
-            raise Exception("Invalid contact type: {}".format(contact_type))
-        raw_str = record[contact_type]
-        author_details_list = []
-        for author_line_str in raw_str.split("\n"):
-            if len(author_line_str) > 0:
-                # print(author_line_str)
-                parts = author_line_str.split("~")
-                author_details = {"Organisation":'',"Position":'',"Address":'',"Email":'',"Role":''}
+        # descriptiveKeywords, keyword, topicCategories
+        #self.parse_column_list(record, 'Keywords (free text)')
+        #self.parse_column_list(record, 'Category')
+        #self.parse_column_list(record, 'Format')
+        #self.parse_bounding_box(record)
 
-                for part in parts:
-                    if ":" in part:                                                
-                        try:
-                            key, value = part.split(":")
-                            author_details[key] = value
-                        except:
-                            raise RecordParseError("Invalid author details: {}".format(part))
+    def parse_responsible_parties(self, record, field, append_mode=False):
+        valid_keys = ['individualName','organizationName','positionName','contactInfo','role','email']
+        responsible_parties = []
+        try:
+            raw_str = record[field]
+            for detail_str in raw_str.split("\n"):
+                if len(detail_str.replace(" ","")) > 0:
+                    detail = {}
+                    for item in detail_str.split("|"):
+                        if 'email' in item and 'contactInfo' in item:
+                            #print(item)
+                            parts = item.split(',')
+                            email_str = parts[-1]
+                            email_k, email_v = email_str.split(":")
+                            addr_str = ','.join(parts[0:len(parts) - 1])
+                            addr_k, addr_v = addr_str.split(":")
+                            email_k = email_k.replace(" ","")
+                            addr_k = addr_k.replace(" ","")
+                            if email_k not in valid_keys or addr_k not in valid_keys:
+                                #print(k)
+                                raise RecordParseError("bad field: %r" % item)
+                            detail[email_k] = email_v
+                            detail[addr_k] = addr_v
+                        else:
+                            parts = item.split(":")
+                            if len(parts) != 2:
+                                raise RecordParseError("bad field: %r" % item)
+                            k,v = item.split(":")
+                            k = k.replace(" ","")
+                            if k not in valid_keys:
+                                #print(k)
+                                raise RecordParseError("bad field: {}".format(item))
+                            detail[k] = v.replace(";","")
+                    responsible_parties.append(detail)
+        except RecordParseError as e:
+            raise RecordParseError("Invalid responible party - {}".format(e))
+        except Exception as e:
+            raise RecordParseError("Invalid responible party - {}".format(item))
 
-                if len(parts[0]) > 0 and ':' not in parts[0]:
-                    author_details["Name"] = parts[0]
-                author_details_list.append(author_details)
-        record[contact_type] = author_details_list    
+        if not append_mode:
+            record['responsibleParties'] = responsible_parties
+        else:
+            record['responsibleParties'] = record['responsibleParties'] + responsible_parties
 
 
     def parse_column_list(self, record, column):
@@ -102,7 +131,7 @@ class MIMSExcelImporter:
                     k,v = part.split(":")
                     k = k.replace(' ','')
                     if k not in parsed_box:
-                        raise
+                        raise Exception()
                 record['Bounding Box'] = parsed_box
             except:
                 raise RecordParseError("Invalid bounding box: {}".format(box_str))
@@ -111,16 +140,16 @@ class MIMSExcelImporter:
 
 if __name__ == "__main__":
     importer = MIMSExcelImporter()
-    imported_records = importer.read_excel_to_json(mims_sheet_file, "Geographic metadata")
+    imported_records = importer.read_excel_to_json(mims_sheet_file, "CKAN_Geographic")
 
-    """
+
     for record in imported_records:
         print("--")
-        for auth in record['Cited Authors']:
+        for auth in record['responsibleParties']:
             #print(auth)
             for k in auth.keys():
                 print("%r : %r" %(k, auth[k]))
             
-        print(record['Bounding Box'])
+        #print(record['Bounding Box'])
         #print("\n")
-    """
+
