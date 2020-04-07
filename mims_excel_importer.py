b7cda1e42858422a1468a28e8a5b75cdef421e0e
kyle-SAEON <kyle@saeon.ac.za>
2020-04-07 08:55:34 +0200

    Fix: Reverted format switching for string to datetime conversion

--- mims_excel_importer.py
+++ mims_excel_importer.py
@@ -259,15 +259,7 @@ if __name__ == "__main__":
                 supported_formats = ["%Y/%m/%d %H:%M","%Y-%m-%d %H:%M:%S","%Y-%m-%d"] #2015/03/12 12:00
                 converted_date = None
                 for fmt in supported_formats:
-                    if len(date_input) == 10: #"%Y-%m-%d"
-                        converted_date = convert_str_to_date(date_input, supported_formats[2])
-                        #break
-                    elif len(date_input) == 16: #"%Y/%m/%d %H:%M"
-                        converted_date = convert_str_to_date(date_input, supported_formats[1])
-                    elif len(date_input) == 19: #"%Y-%m-%d %H:%M:%S"
-                        converted_date = convert_str_to_date(date_input, supported_formats[0])
-                    else:
-                        print('Unable to determine date format')
+                    converted_date = convert_str_to_date(date_input, fmt)
                 return converted_date
 
         start_time = record['startTime']
